3b38c19dabb14c17c56b30162db0f32c33cfcee1
ani_ghost <57623270+anighost1@users.noreply.github.com>
2024-01-25 16:41:32 +0530

    search feature implemented

--- services/config.js
+++ services/config.js
@@ -1,8 +1,8 @@
 
 import http from "./http";
 
-const getItemsWithImg = (page = 1, limit = 10) => {
-    return http.get(`/api/item/with-image?page=${page}&limit=${limit}`)
+const getItemsWithImg = (page = 1, limit = 10, search='') => {
+    return http.get(`/api/item/with-image?page=${page}&limit=${limit}&search=${search}`)
 }
 
 const getItemByIdWithImg = (id) => {
